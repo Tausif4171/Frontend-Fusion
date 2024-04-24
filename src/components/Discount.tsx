876660110e9c9b22f16ad9a5fc39a6111db659fa
Tausif Khan <tausifkhan4171@gmail.com>
2024-04-24 18:26:27 +0530

    discount component

--- src/components/Discount.tsx
+++ src/components/Discount.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import emptyImage from '../assets/emptyImage.svg'
 
 function Discount() {
     return (
-        <div>Discount</div>
+        <div className='my-[27px] mx-[20px]'>
+            <div className='flex justify-between px-[22px] items-center bg-[#F9B023] w-[269px] h-[123px] rounded-[18px]'>
+                <div>
+                    <img src={emptyImage} />
+                </div>
+                <div className='text-[#FFFFFF]'>
+                    <p className='font-light text-[20px] leading-[27.32px]'>Get</p>
+                    <p className='text-[26px] leading-[35.52px] font-extrabold'>50% OFF</p>
+                    <p className='text-[13px] font-medium leading-[17.76px]'>On first 03 order</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Discount from '../components/Discount'
 
 function Home() {
     return (
         <div>
             <Header />
+            <Discount />
         </div>
     )
 }
