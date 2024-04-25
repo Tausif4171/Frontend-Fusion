91e4651b5fb724e84154cf7b6c31db3abfd9533c
Tausif Khan <tausifkhan4171@gmail.com>
2024-04-25 21:20:23 +0530

    product display from an enpoint

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import emptyImage from '../assets/emptyImage.svg'
-import plus from '../assets/plus.svg'
-import heart from '../assets/heart.svg'
-import heartFilled from '../assets/heartFilled.svg'
-import Product from '../interfaces/Product'
+import React from 'react';
+import emptyImage from '../assets/emptyImage.svg';
+import plus from '../assets/plus.svg';
+import heart from '../assets/heart.svg';
+import heartFilled from '../assets/heartFilled.svg';
+import Product from '../interfaces/Product';
 
 interface ProductItemProps {
     product: Product;
@@ -15,23 +15,22 @@ function ProductItem(props: ProductItemProps) {
     return (
         <div className='bg-[#F8F9FB] py-5 px-[17px] w-[160px] h-[194px] rounded-xl relative'>
             <div className='absolute top-[13px] left-[13px]'>
-                <img src={heart} />
+                <img src={heart} alt="Favorite" />
             </div>
             <div className='flex justify-center'>
-                <img src={emptyImage} />
+                <img className='w-[68px] h-[68px]' src={product.thumbnail || emptyImage} alt="Product" />
             </div>
             <div className='flex justify-between mt-[56px]'>
                 <div>
-                    <p className='text-[#1E222B] font-semibold text-[14px] leading-5'>$325</p>
-                    <p className='text-[#616A7D] font-normal text-[12px] leading-4'>Clown Tang.H03</p>
+                    <p className='text-[#1E222B] font-semibold text-[14px] leading-5'>${product.price}</p>
+                    <p className='text-[#616A7D] font-normal text-[12px] leading-4'>{product.title}</p>
                 </div>
                 <div className='bg-[#2A4BA0] w-6 h-6 flex justify-center items-center rounded-[50%] '>
-                    <img src={plus} />
+                    <img src={plus} alt="Add to Cart" />
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
 
-export default ProductItem
+export default ProductItem;
